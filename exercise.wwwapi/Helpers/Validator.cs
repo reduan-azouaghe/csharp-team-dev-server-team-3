37b75c3889fa88f4f1fcf6a8111d85ec1578f8b5
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-08-29 15:11:38 +0200

    Finished password validation method

--- exercise.wwwapi/Helpers/Validator.cs
+++ exercise.wwwapi/Helpers/Validator.cs
@@ -1,19 +1,27 @@
-﻿namespace exercise.wwwapi.Helpers
+﻿using Microsoft.Extensions.FileSystemGlobbing.Internal;
+using System.Text.RegularExpressions;
+
+namespace exercise.wwwapi.Helpers
 {
     public class Validator
     {
-        public bool Password(string passwordString)
+        public string Password(string passwordString)
         {
-            throw new NotImplementedException();
             //TODO: Not less than 8 characters       | Done
-            //TODO: Atleast one uppercase            | 
-            //TODO: Atleast one number               |
-            //TODO: Atleast one special character    |
+            //TODO: Atleast one uppercase            | Done
+            //TODO: Atleast one number               | Done
+            //TODO: Atleast one special character    | Done
 
-            if (passwordString.Count() < 8) return false;
-            if (!passwordString.Any(char.IsUpper)) return false;
-            if (!passwordString.Any(char.IsNumber)) return false;
-            if (!passwordString.Any(char.is)) return false;
+            if (passwordString.Count() < 8) return "Too few characters";
+
+            if (!passwordString.Any(char.IsUpper)) return "Missing uppercase characters";
+
+            if (!passwordString.Any(char.IsNumber)) return "Missing number(s) in password";
+
+            // Accept only !@#$%^&*()-_=+{};:',./\|~
+            string regexPattern = ".*[!@#$%^&*()\\-_=+{};:',./\\\\|~].*";
+            if (!Regex.IsMatch(passwordString, regexPattern)) return "Missing special character";
+            return "Accepted";
         }
     }
 }

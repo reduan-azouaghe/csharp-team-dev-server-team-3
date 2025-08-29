39c45a7e7093a3a043cd2e7e1c3a26d94d66c8a6
Oyvind Timian Dokk Husveg <oyvind.timian.dokk.husveg@no.experis.com>
2025-08-29 13:24:21 +0200

    Started work on password validation

---
+++ exercise.wwwapi/Helpers/Validator.cs
@@ -0,0 +1,18 @@
+﻿namespace exercise.wwwapi.Helpers
+{
+    public class Validator
+    {
+        public bool Password(string passwordString)
+        {
+            //TODO: Not less than 8 characters       | Done
+            //TODO: Atleast one uppercase            | 
+            //TODO: Atleast one number               |
+            //TODO: Atleast one special character    |
+
+            if (passwordString.Count() < 8) return false;
+            if (!passwordString.Any(char.IsUpper)) return false;
+            if (!passwordString.Any(char.IsNumber)) return false;
+            if (!passwordString.Any(char.is)) return false;
+        }
+    }
+}
